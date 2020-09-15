ae87c352394565cfaa1ca1426d77185dd98d8605
EdwinPB <epolo@wawand.co>
2020-09-14 22:35:02 -0500

    added new css styles

--- assets/js/application.js
+++ assets/js/application.js
@@ -2,5 +2,16 @@ require("expose-loader?$!expose-loader?jQuery!jquery");
 require("bootstrap/dist/js/bootstrap.bundle.js");
 
 $(() => {
-
+    $(document).on('click', 'input[type="checkbox"]', function (event) {
+        if($(this).prop("checked") == true){
+            console.log("Checkbox is checked.", $(this).attr("id"));
+        }
+        else if($(this).prop("checked") == false){
+            console.log("Checkbox is unchecked.");
+        }
+    });
 });
+
+
+
+
