a91b7c1aee5f32fc1cf25f987d1bf573b384ed87
EdwinPB <epolo@wawand.co>
2020-09-15 23:28:20 -0500

    added styles for input

--- actions/calculators.go
+++ actions/calculators.go
@@ -9,23 +9,13 @@ import (
 
 // CalculatorsShow default implementation.
 func CalculatorsShow(c buffalo.Context) error {
-	theme := c.Params().Get("theme")
-	if theme == "" {
-		theme = "day"
-	}
-
 	c.Set("result", "")
-	c.Set("theme", theme)
+	c.Set("theme", c.Params().Get("theme"))
 	return c.Render(http.StatusOK, r.HTML("calculators/show.html"))
 }
 
 // CalculatorsCalculate default implementation.
 func CalculatorsCalculate(c buffalo.Context) error {
-	theme := c.Params().Get("theme")
-	if theme == "" {
-		theme = "day"
-	}
-
 	calculator := models.Calculator{}
 	if err := c.Bind(&calculator); err != nil {
 		return err
@@ -33,6 +23,6 @@ func CalculatorsCalculate(c buffalo.Context) error {
 	result, _ := models.Calculate(calculator.EnteredValue)
 
 	c.Set("result", result)
-	c.Set("theme", theme)
+	c.Set("theme", c.Params().Get("theme"))
 	return c.Render(http.StatusOK, r.HTML("calculators/show.html"))
 }
